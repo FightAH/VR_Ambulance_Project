a80d64d0764956055022df7969dae8769346b498
FightAH <34405770+FightAH@users.noreply.github.com>
2019-11-25 10:43:57 +0100

    Added watch, time and sound
    Added watch, time and sound

---
+++ Assets/Scripts/WatchTime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WatchTime : MonoBehaviour
+{
+
+    public float timeLeft = 50.0f;
+    public Text startText; // used for showing countdown from 3, 2, 1 
+    int Stop = 1;
+
+
+    void Update()
+    {
+        if (Stop == 1)
+        {
+            timeLeft -= Time.deltaTime;
+            int min = Mathf.FloorToInt(timeLeft / 60);
+            int sec = Mathf.FloorToInt(timeLeft % 60);
+            startText.GetComponent<UnityEngine.UI.Text>().text = min.ToString("00") + ":" + sec.ToString("00");
+            if (timeLeft < 0)
+            {
+                Stop = 0;
+                Debug.Log("Time up");
+            }
+        }
+    }
+}
