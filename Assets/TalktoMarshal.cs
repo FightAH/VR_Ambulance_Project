5bc593c29b9a6f9706e5456f550b44a1b105c39b
FightAH <34405770+FightAH@users.noreply.github.com>
2019-11-29 11:51:47 +0100

    Talk to Marshal

---
+++ Assets/TalktoMarshal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalktoMarshal : MonoBehaviour {
+
+    public static int marshal = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        marshal = 1;
+        Debug.Log("talking");
+    }
+}
